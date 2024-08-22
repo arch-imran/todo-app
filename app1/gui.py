4872bd09106d71a623220d9a6dd1161847e9668a
imran <mdimran82076@gmail.com>
2024-08-22 15:02:02 +0530

    edit feature and complete feature

--- app1/gui.py
+++ app1/gui.py
@@ -5,8 +5,18 @@ label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter a todo",key='todo')
 add_button = sg.Button("Add")
 
+complete_button = sg.Button("Complete")
+
+#display the todo in a list box
+
+list_box = sg.Listbox(values=functions.get_todo(),
+                      key='existing_todo',
+                      enable_events=True,size=[45,10])
+edit_button = sg.Button("Edit")
+
 window = sg.Window("My to do list app",
-                   layout=[[label,input_box,add_button]],
+                   layout=[[label,input_box,add_button,complete_button],
+                           [list_box,edit_button]],
                    font = ('Helvetica',10))
 
 while True:
@@ -18,8 +28,44 @@ while True:
         todos.append(new_todo)
         functions.write_todo(todos)
 
+        #updating the list box
+        window['existing_todo'].update(values=todos)
+
+
+    elif event == "Edit":
+        todos = functions.get_todo()
+
+        new_todo = values['todo'] +'\n'
+        todo_to_edit = values["existing_todo"][0]
+        index = todos.index(todo_to_edit)
+        #updating the todo
+        todos[index]=new_todo
+
+        #storing todo changed in file
+        functions.write_todo(todos)
+
+        #updating the list box
+        window['existing_todo'].update(values=todos)
+
+    elif event=="existing_todo":
+        #on clicking the todo it should appear in text add
+        window['todo'].update(value=values['existing_todo'][0])
+
+    elif event == 'Complete':
+        todo_completed = values['existing_todo'][0]
+
+        todos = functions.get_todo()
+
+        index = todos.index(todo_completed)
+
+        todos.pop(index)
+        functions.write_todo(todos)
+
+        #updating the list box
+        window['existing_todo'].update(values=todos)
     elif event == sg.WINDOW_CLOSED:
         break
+
 
 
 
